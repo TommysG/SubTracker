d45450b38bb9d0baff40fdc6a5d81dab4d3fa388
Tommys Gian <tommysg@manjaro.station>
2020-11-20 23:40:50 +0200

    add subs page

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import home from "./Pages/home.js";
-import { MaterialIcons } from '@expo/vector-icons';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Pages/Home";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -12,28 +12,36 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="History" component={home}
-         options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="history" size={20} color="white" />
-          ),
-          }}/>
-        <Tab.Screen name="Subs" component={home} 
+        <Tab.Screen
+          name="History"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="history" size={20} color="white" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Subs"
+          component={Home}
           options={{
             tabBarIcon: ({ color }) => (
               <MaterialIcons name="library-books" size={20} color="white" />
             ),
-          }}/>
-        <Tab.Screen name="Profile" component={home} 
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Home}
           options={{
             tabBarIcon: ({ color }) => (
               <MaterialIcons name="person-outline" size={24} color="white" />
             ),
-          }}/>
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-  );  
+  );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
